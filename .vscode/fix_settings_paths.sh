7005185998b7f641a73c65ca3234c932aff23b22
giacomopj <giacomo.pojani@spire.com>
2022-02-23 01:06:13 +0000

    Minor fix to script for settings.json

--- .vscode/fix_settings_paths.sh
+++ .vscode/fix_settings_paths.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
-env=$(poetry env info --path)
+# Regenerate settings.json with Poetry environment folder
+# by using the absolute path instead of the relative one
+
+env_path=$(poetry env info --path)
 
 echo $env
 
 echo "{
-    \"python.pythonPath\": \"${env}/bin/python\",
+    \"python.pythonPath\": \"${env_path}/bin/python\",
     \"python.languageServer\": \"Pylance\",
-    \"python.linting.mypyPath\": \"${env}/bin/mypy\",
-	\"python.linting.flake8Path\": \"${env}/bin/flake8\",
+    \"python.linting.mypyPath\": \"${env_path}/bin/mypy\",
+	\"python.linting.flake8Path\": \"${env_path}/bin/flake8\",
 	\"python.linting.mypyEnabled\": true,
 	\"python.linting.flake8Enabled\": true,
     \"python.linting.pylintEnabled\": false,
     \"python.linting.pycodestyleEnabled\": false,
-    \"python.formatting.blackPath\": \"${env}/bin/black\",
+    \"python.formatting.blackPath\": \"${env_path}/bin/black\",
     \"python.formatting.provider\": \"black\",
 	\"python.formatting.blackArgs\": [\"--line-length\", \"79\"],
-    \"python.testing.pytestPath\": \"${env}/bin/pytest\",
+    \"python.testing.pytestPath\": \"${env_path}/bin/pytest\",
     \"python.testing.unittestEnabled\": false,
     \"python.testing.pytestEnabled\": true,
     \"python.testing.pytestArgs\": [
