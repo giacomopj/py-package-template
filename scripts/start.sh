ce4ddfe742cdd4ae9818066f33c77201d6b8af31
giacomopj <giacomo.pojani@spire.com>
2022-03-03 20:09:03 +0000

    Added unit tests in start-up

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,6 +7,11 @@ set -e
 printf "\nRunning code pre-commit checks\n"
 pre-commit run --all-files
 
+# Run all unit tests
+printf "\nRunning unit tests\n"
+pytest
+pytest --cov --cov-fail-under=100
+
 # Run the application executable
 printf "\nRunning application executable with example parameter\n"
 python -m src Tester Container
