610addb6d4926e08a64bcdf9b09ccc6299afde05
giacomopj <giacomo.pj@hotmail.it>
2022-03-31 19:37:51 +0100

    Fix for Windows users

---
+++ .vscode/fix_tasks_windows.ps1
@@ -0,0 +1,19 @@
+Write-Host "Fix environment variables in tasks.json for Windows users"
+
+$a = Read-Host -Prompt "Input personal Git access token"
+
+Write-Host "Git access token =" $a
+
+Write-Host "Workspace path set to parent of current working directory"
+
+$curDir = Get-Location
+$b = Split-Path -Parent $curDir
+
+Write-Host "Workspace path =" $b
+
+$in = Get-Content "tasks.json"
+$in | foreach {$_.replace('${GIT_ACCESS_TOKEN}',$a)} | foreach {$_.replace('${config:GIT_ACCESS_TOKEN}',$a)} | Out-File -encoding ASCII "tasks.json"
+$in = Get-Content "tasks.json"
+$in | foreach {$_.replace('${fileWorkspaceFolder}',$b)} | foreach {$_.replace('${WORKSPACE_PATH}',$b)} | Out-File -encoding ASCII "tasks.json"
+
+Write-Host "File tasks.json fixed"
